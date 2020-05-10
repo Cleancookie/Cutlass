46293f198e1ce3dc8d55d478c4818510733c8474
Alex Law <pokemon999101@hotmail.co.uk>
2020-05-10 22:44:33 +0100

    ✨ Enter TailwindCSS and Twing

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 const {XdccClient, XdccEvents} = require('irc-xdcc-2');
+const app = require('express')();
+const { TwingEnvironment, TwingLoaderFilesystem } = require('twing');
+let loader = new TwingLoaderFilesystem('./views');
+let twing = new TwingEnvironment(loader);
+
+const ircOptions = {
+    server: 'irc.rizon.net'
+    , nick: 'Cookie_____'
+    , userName: 'Cookie_____'
+    , realName: 'Clean Cookie'
+    , port: 6697
+    , autoRejoin: true
+    , autoConnect: true
+    , channels: ['#NIBL']
+    , secure: true
+    , selfSigned: true
+    , certExpired: true
+    , stripColors: true
+    , encoding: 'UTF-8'
+    // xdcc specific options
+    , progressInterval: 1
+    , destPath: './unprocessed'
+    , resume: false
+    , acceptUnpooled: true
+    , closeConnectionOnCompleted: false
+};
+
+app.get('/', async (req, res) => {
+    try {
+        const view = await twing.render('homepage.twig');
+        res.end(view);
+    } catch (e) {
+        console.log(e);
+    }
+});
+app.listen(3000);
 
 async function main() {
-    // load irc-xdcc module
-    // set options object
-    const ircOptions = {
-        server: 'irc.rizon.net'
-        , nick: 'Cookie_____'
-        , userName: 'Cookie_____'
-        , realName: 'Clean Cookie'
-        , port: 6697
-        , autoRejoin: true
-        , autoConnect: true
-        , channels: ['#NIBL']
-        , secure: true
-        , selfSigned: true
-        , certExpired: true
-        , stripColors: true
-        , encoding: 'UTF-8'
-        // xdcc specific options
-        , progressInterval: 1
-        , destPath: './unprocessed'
-        , resume: false
-        , acceptUnpooled: true
-        , closeConnectionOnCompleted: false
-    };
-
     try {
         const client = await XdccClient.create(ircOptions);
         console.log('Connected');
@@ -51,4 +63,4 @@ async function ircPmHandler(nick, text, message) {
     // client.on(XdccEvents.xdccError, (error) => { console.log(error) })
 }
 
-main();
+// main();
